b4fa5fc895e713a268ee7dbe82b2fc86f83a3079
Cephas-05 <cephasprincely5@gmail.com>
2024-09-30 19:41:24 +0530

    initial check commit

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from items.models import Category, Item
+
+from .forms import SignupForm
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request,"core/index.html", {"items":items, "categories":categories})
+
+
+def contact(request):
+    return render(request, "core/contact.html")
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'core/signup.html',{'form': form})
